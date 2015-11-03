aafd486b04ea5ef14b47bd171950734949e89c29
Cyril Milleret <cyril.milleret@gmail.com>
2015-11-03 14:46:24 +0100

    new TRI using focal function

--- R/TRI.R
+++ R/TRI.R
@@ -1,123 +1,24 @@
 #' Compute a terrain ruggedness index followin sappington et al 2007
 #' 
-#' it returns the sum of the absolute values of the elevation differences between a central grid cell and its 8 neighboring cells
+#' it returns a raster layer with the sum of the absolute values of the elevation differences between a central grid cell and its 8 neighboring cells
 #' 
 #' @author Cyril Milleret 
-#' @param r is the SpatialPixel(DataFrame)
-#' @param layer the layer where elevation is stored (for instance r$elevation)
-#' @usage Terrain <- TRI(r= veg_mask,layer=veg_mask$elevation)
+#' @param r1 is the raster layer to compute TRI from
+#' @param gri is the resolution of the grid in which TRI should be calculated. grid=3 will look like this :
+#'        1,1,1,
+#'        1,0,1,
+#'        1.1,1  where 0 is the focal cell where the index is calculated and 1 the cells use to calculated the relative index
+#' @usage Terrain <- TRI(r1= veg_mask,res=3)
 #' @export 
 
-TRI <- function(r=r,layer=r$layer){
-  require(sp)
-  temp<-as.data.frame(r@grid)
+TRI<-function(r1,gri=3){
+  m <- matrix(c(1:(gri*gri)), byrow=T,nrow=gri)
+  gri <- 3## griolution of the TRI
+  foc <- median(c(1:(gri*gri)))
   
-  row<-temp[1,3]## number of cells on the row
-  col<-temp[2,3]## number of cells on the columns
+  func <- function(x) (sum(abs((x[foc]- x[c(1:(foc-1),(foc+1):(gri*gri))])))) 
+  r2<-focal(r1,m,fun=func)
   
-  
-  notdo_rigth <- row*c(1:col)
-  notdo_left<-seq(1,row*col,by=row)
-  notdo_above<-seq(1,row,by=1)
-  notdo_below<-seq(row*col-row+2 ,row*col-1,by=1)
-  
-  all_<-c(notdo_rigth,notdo_left,notdo_above,notdo_below)
-  all_<-unique(all_)
-  
-  ## erase some
-  notdo_rigth<- notdo_rigth[!notdo_rigth %in% c(row,row*col) ] # is not in 
-  notdo_left<- notdo_left[!notdo_left %in% c(1,row*col-row+1) ] # is not in 
-  notdo_above<- notdo_above[!notdo_above %in% c(1,row) ] # is not in 
-  
-  
-  TRI <- NULL
-  for ( i in 1:length(layer)){
-    
-    if(!i %in% all_){ 
-      TRI[i]<- sum(abs(
-        layer[i-1] - layer[i]),
-        abs(layer[i+1] - layer[i]),
-        
-        abs(layer[i-row+1] - layer[i]),
-        abs(layer[i-row] - layer[i]),
-        abs(layer[i-row-1] - layer[i]), 
-        
-        abs(layer[i+row+1] - layer[i]),
-        abs(layer[i+row] - layer[i]),
-        abs(layer[i+row-1] - layer[i]))
-    } else{
-      
-      
-      if(i %in% notdo_rigth){
-        TRI[i]<- sum(abs(
-          layer[i-1] - layer[i]),
-          abs(layer[i-row] - layer[i]),
-          abs(layer[i-row-1] - layer[i]),
-          abs(layer[i+row] - layer[i]),
-          abs(layer[i+row-1] - layer[i]))
-      } else{
-        
-        if(i %in% notdo_left){
-          TRI[i]<- sum(abs(
-            layer[i+1] - layer[i]),
-            abs(layer[i-row] - layer[i]),
-            abs(layer[i-row+1] - layer[i]),
-            abs(layer[i+row] - layer[i]),
-            abs(layer[i+row+1] - layer[i]))
-        } else{
-          
-          if(i %in% notdo_above){
-            TRI[i]<- sum(abs(
-              layer[i-1] - layer[i]),
-              abs(layer[i+1] - layer[i]),
-              abs(layer[i+row+1] - layer[i]),
-              abs(layer[i+row] - layer[i]),
-              abs(layer[i+row-1] - layer[i]))
-          } else{
-            
-            if(i %in% notdo_below){
-              TRI[i]<- sum(abs(
-                layer[i-1] - layer[i]),
-                abs(layer[i+1] - layer[i]),
-                abs(layer[i-row+1] - layer[i]),
-                abs(layer[i-row] - layer[i]),
-                abs(layer[i-row-1] - layer[i]))
-            }  else{
-              
-              
-              
-              
-              
-              if(i ==row){# rigth corner up
-                TRI[i]<- sum(
-                  abs(layer[i-1] - layer[i]),
-                  abs(layer[i+row] - layer[i]),
-                  abs(layer[i+row-1] - layer[i])
-                )
-              } else{
-                
-                if(i == 1){# left corner up
-                  TRI[i]<- sum(abs(
-                    layer[i+1] - layer[i]),
-                    abs(layer[i+row] - layer[i]),
-                    abs(layer[i+row+1] - layer[i]))
-                } else{
-                  
-                  if(i == row*col-row+1){# left corner down
-                    TRI[i]<- sum(abs(
-                      layer[i+1] - layer[i]),
-                      abs(layer[i-row] - layer[i]),
-                      abs(layer[i-row+1] - layer[i]))
-                  } else{
-                    
-                    if(i == row*col){# rigth corner down
-                      TRI[i]<- sum(abs(
-                        layer[i-1] - layer[i]),
-                        abs(layer[i-row] - layer[i]),
-                        abs(layer[i-row-1] - layer[i]))
-                    }
-                  }}}}}}}}
-  }
-  
-  return(TRI)
+  return(r2)
 }
+
